d442dff66eb1cc3b6a977ebc2b9f84489506e035
Lucas Ragazzi Pereira Maciel <lucas.maciel@sottelli.com>
2024-01-23 14:44:46 -0300

    add contact in account

--- force-app/main/default/lwc/lwcTeste/lwcTeste.js
+++ force-app/main/default/lwc/lwcTeste/lwcTeste.js
@@ -1,4 +1,4 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 import conObject from '@salesforce/schema/Contact';
 import conFirstName from '@salesforce/schema/Contact.FirstName';
@@ -9,9 +9,11 @@ import conDepartment from '@salesforce/schema/Contact.Department';
 import conCpf from '@salesforce/schema/Contact.Cpf__c';
 import conCnpj from '@salesforce/schema/Contact.Cnpj__c';
 import conPe from '@salesforce/schema/Contact.Tipo_Pessoa__c';
-
-
+import ACCOUNT_FIELD from '@salesforce/schema/Contact.AccountId'; // Campo de referência à conta
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getAccountOptions from '@salesforce/apex/AccountController.getAccountOptions';
 
 export default class ContactForm extends LightningElement {
     @track firstName = '';
@@ -26,10 +28,20 @@ export default class ContactForm extends LightningElement {
     @track isPessoaJuridica = false;
     @track cpf = '';
     @track cnpj = '';
+    @track accountOptions = [];
+    @track selectedAccountId = '';
 
-
-
-
+    @wire(getAccountOptions)
+    wiredAccountOptions({ error, data }) {
+        if (data) {
+            this.accountOptions = data.map(option => ({
+                label: option.accountName,
+                value: option.accountId
+            }));
+        } else if (error) {
+            console.error(error);
+        }
+    }
     get options() {
         return [
             { label: 'Pessoa Física', value: 'Pessoa Física' },
@@ -37,11 +49,21 @@ export default class ContactForm extends LightningElement {
         ];
     }
 
+    handleAccountChange(event) {
+        this.selectedAccountId = event.detail.value;
+    }
+
     handleChange(event) {
         this.value = event.detail.value;
         this.isFormVisible = true;
         this.isPessoaFisica = this.value === 'Pessoa Física';
         this.isPessoaJuridica = this.value === 'Pessoa Jurídica';
+    }
+
+    handleTipoPessoaChange(event) {
+        this.tipoPessoa = event.detail.value;
+        this.tipoPessoaSelected = true;
+        this.tipoPessoaText = `Tipo de Pessoa: ${this.tipoPessoa}`;
     }
 
     contactChangeVal(event) {
@@ -52,12 +74,11 @@ export default class ContactForm extends LightningElement {
     }
 
     insertContactAction() {
-        // Verifique se todos os campos estão preenchidos
-        if (!this.firstName || !this.lastName || !this.bday || !this.emailId || !this.departmentVal || !this.value || (!this.cpf && !this.cnpj)) {
+        if (!this.firstName || !this.lastName || !this.bday || !this.emailId || !this.departmentVal || !this.value || (!this.cpf && !this.cnpj) || !this.selectedAccountId) {
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Erro',
-                    message: 'Preencha os campos',
+                    message: 'Preencha todos os campos, incluindo a conta',
                     variant: 'error',
                 }),
             );
@@ -70,9 +91,11 @@ export default class ContactForm extends LightningElement {
         fields[conBday.fieldApiName] = this.bday;
         fields[conEmail.fieldApiName] = this.emailId;
         fields[conDepartment.fieldApiName] = this.departmentVal;
-        fields[conPe.fieldApiName] = this.value;
+        fields[conPe.fieldApiName] = this.tipoPessoa;
         fields[conCpf.fieldApiName] = this.cpf;
         fields[conCnpj.fieldApiName] = this.cnpj;
+        fields[ACCOUNT_FIELD.fieldApiName] = this.selectedAccountId;
+
         const recordInput = { apiName: conObject.objectApiName, fields };
 
         createRecord(recordInput)
@@ -80,7 +103,7 @@ export default class ContactForm extends LightningElement {
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'Success',
-                        message: 'Contact record has been created',
+                        message: 'Registro de Contato foi criado',
                         variant: 'success',
                     }),
                 );
@@ -88,11 +111,11 @@ export default class ContactForm extends LightningElement {
             .catch(error => {
                 this.dispatchEvent(
                     new ShowToastEvent({
-                        title: 'Error creating record',
+                        title: 'Erro ao criar registro',
                         message: error.body.message,
                         variant: 'error',
                     }),
                 );
             });
     }
-}    
+}
